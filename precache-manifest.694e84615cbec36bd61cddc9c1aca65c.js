c0ce917b14e3e86aa29461d6ca8436c5a7815a42
roy liao <roy_y_liao@hotmail.com>
2020-03-02 12:30:12 +1100

    Updates

---
+++ precache-manifest.694e84615cbec36bd61cddc9c1aca65c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec312ae6826d2e70af985af3992c565",
+    "url": "/books-app/index.html"
+  },
+  {
+    "revision": "a0f142abf2c2e4f80fab",
+    "url": "/books-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3cd4a357394e72b52acc",
+    "url": "/books-app/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "a0f142abf2c2e4f80fab",
+    "url": "/books-app/static/js/2.25bd4714.chunk.js"
+  },
+  {
+    "revision": "3cd4a357394e72b52acc",
+    "url": "/books-app/static/js/main.05e89c63.chunk.js"
+  },
+  {
+    "revision": "e7336500a6bec4f27f17",
+    "url": "/books-app/static/js/runtime~main.6efe4f9a.js"
+  }
+]);
